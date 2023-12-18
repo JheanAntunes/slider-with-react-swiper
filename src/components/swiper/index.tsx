c469ad318e44224de7aca41a892b5a748c160e7d
jhean <brizadaojhean@gmail.com>
2023-12-18 20:46:13 -0300

    :lipstick: styles: prettier

--- src/components/swiper/index.tsx
+++ src/components/swiper/index.tsx
@@ -1,48 +1,45 @@
-"use client"
-import { useEffect, useRef } from 'react';
-import { register } from 'swiper/element/bundle';
-import type { SwiperSlideProps, SwiperProps } from 'swiper/react';
-export function Swiper(props:SwiperProps) {
-  const swiperRef = useRef(null);
-  const {
-    children,
-    ...rest
-  } = props;
+'use client'
+import { useEffect, useRef } from 'react'
+import { register } from 'swiper/element/bundle'
+import type { SwiperProps, SwiperSlideProps } from 'swiper/react'
+export function Swiper(props: SwiperProps) {
+  const swiperRef = useRef(null)
+  const { children, ...rest } = props
 
   useEffect(() => {
     // Register Swiper web component
-    register();
+    register()
 
     // pass component props to parameters
     const params = {
-      ...rest
-    };
+      ...rest,
+    }
 
     // Assign it to swiper element
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    Object.assign(swiperRef.current, params);
+    Object.assign(swiperRef.current, params)
 
     // initialize swiper
-     // @ts-ignore
-    swiperRef.current.initialize();
-  }, [rest]);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    swiperRef.current.initialize()
+  }, [rest])
 
   return (
     <swiper-container init={false} ref={swiperRef}>
       {children}
     </swiper-container>
-  );
+  )
 }
-export function SwiperSlide(props:SwiperSlideProps) {
-  const {
-    children,
-    ...rest
-  } = props;
+export function SwiperSlide(props: SwiperSlideProps) {
+  const { children, ...rest } = props
 
   return (
     <swiper-slide {...rest}>
+      {/*eslint-disable-next-line @typescript-eslint/ban-ts-comment*/}
       {/* @ts-ignore */}
       {children}
     </swiper-slide>
-  );
+  )
 }
