aaf6954dc916be7442ef7821a2bc517de5a305a2
jhean <brizadaojhean@gmail.com>
2023-12-19 20:40:59 -0300

    :lipstick:	styles: eslint-prettier

--- swiper.d.ts
+++ swiper.d.ts
@@ -1,18 +1,18 @@
-import React from "react";
+import React from 'react'
 
-import type { SwiperSlideProps, SwiperProps } from 'swiper/react';
+import type { SwiperProps, SwiperSlideProps } from 'swiper/react'
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      "swiper-container": React.DetailedHTMLProps<
+      'swiper-container': React.DetailedHTMLProps<
         React.HTMLAttributes<HTMLElement> & SwiperProps,
         HTMLElement
-      >;
-      "swiper-slide": React.DetailedHTMLProps<
+      >
+      'swiper-slide': React.DetailedHTMLProps<
         React.HTMLAttributes<HTMLElement> & SwiperSlideProps,
         HTMLElement
-      >;
+      >
     }
   }
 }
