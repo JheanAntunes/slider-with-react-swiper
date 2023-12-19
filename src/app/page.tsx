eeedf3c93f0063d33b639d6531f7ae43cbff5917
jhean <brizadaojhean@gmail.com>
2023-12-19 20:43:11 -0300

    :sparkles:	feat: import do component example-slider-swiper-responsive na home-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import ExampleSliderSwiperAutoScroll from '@/components/ui/example-slider-swiper-auto-scroll'
 import ExampleSliderSwiperBasic from '@/components/ui/example-slider-swiper-basic'
+import ExampleSliderSwiperResponsive from '@/components/ui/example-slider-swiper-responsive'
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-8 py-8">
       <ExampleSliderSwiperBasic />
       <ExampleSliderSwiperAutoScroll />
+      <ExampleSliderSwiperResponsive />
     </main>
   )
 }
