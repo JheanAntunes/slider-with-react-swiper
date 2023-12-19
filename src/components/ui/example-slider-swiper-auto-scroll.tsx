61882c116b95c9a63d4bca0ac30a5257a075308e
jhean <brizadaojhean@gmail.com>
2023-12-18 23:21:52 -0300

    :sparkles:	feat: exemplo de slider com auto-scroll automatico

---
+++ src/components/ui/example-slider-swiper-auto-scroll.tsx
@@ -0,0 +1,28 @@
+import 'swiper/css'
+import { Swiper } from '../swiper'
+import { TypographyH1 } from '../typography'
+import GenerateSwiperSliders from '../utils/generate-swiper-sliders'
+function ExampleSliderSwiperAutoScroll() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-8">
+      <TypographyH1>Slider auto-scroll</TypographyH1>
+      <div className="w-full max-w-4xl">
+        <Swiper
+          autoplay={true}
+          speed={500}
+          loop={true}
+          slidesPerView={3}
+          spaceBetween={32}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+        >
+          <GenerateSwiperSliders />
+        </Swiper>
+      </div>
+    </section>
+  )
+}
+
+export default ExampleSliderSwiperAutoScroll
